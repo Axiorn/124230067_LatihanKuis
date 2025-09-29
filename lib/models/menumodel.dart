c5511fe20ad25768124d3ea6b680870cb419b4cb
latief <80023698+Axiorn@users.noreply.github.com>
2025-09-29 23:51:25 +0700

    create model menu

---
+++ lib/models/menumodel.dart
@@ -0,0 +1,15 @@
+class Menu {
+  String name;
+  String desc;
+  List<String> kategori;
+  String price;
+  List<String> imageassets;
+
+  Menu({
+    required this.name,
+    required this.desc,
+    required this.kategori,
+    required this.price,
+    required this.imageassets,
+  });
+}
