4eebe490865729e644faa5c50699805ba5e0e405
latief <80023698+Axiorn@users.noreply.github.com>
2025-09-29 23:51:04 +0700

    make data file

---
+++ lib/data/menudata.dart
@@ -0,0 +1,58 @@
+import 'package:latkuis_ppam/models/menumodel.dart';
+
+final List<Menu> menuList = [
+  Menu(
+    name: "Nasi Goreng Spesial",
+    desc: "Nasi goreng dengan telur, ayam suwir, dan kerupuk.",
+    kategori: ["Makanan", "Nasi", "Spesial"],
+    price: "25000",
+    imageassets: [
+      "assets/images/nasigoreng1.png",
+    ],
+  ),
+  Menu(
+    name: "Es Teh Manis",
+    desc: "Teh segar dengan gula, disajikan dingin.",
+    kategori: ["Minuman", "Dingin"],
+    price: "5000",
+    imageassets: [
+      "assets/images/esteh1.png",
+    ],
+  ),
+  Menu(
+    name: "Ayam Geprek Keju",
+    desc: "Ayam crispy dengan sambal pedas ditambah lelehan keju.",
+    kategori: ["Makanan", "Ayam", "Pedas"],
+    price: "28000",
+    imageassets: [
+      "assets/images/geprek1.png",
+    ],
+  ),
+  Menu(
+    name: "Mie Ayam Bakso",
+    desc: "Mie ayam dengan topping bakso sapi kenyal.",
+    kategori: ["Makanan", "Mie"],
+    price: "20000",
+    imageassets: [
+      "assets/images/mieayam1.png",
+    ],
+  ),
+  Menu(
+    name: "Jus Alpukat",
+    desc: "Jus alpukat kental dengan susu cokelat.",
+    kategori: ["Minuman", "Jus"],
+    price: "15000",
+    imageassets: [
+      "assets/images/jusalpukat.png",
+    ],
+  ),
+  Menu(
+    name: "Sate Ayam",
+    desc: "Sate ayam dengan bumbu kacang dan lontong.",
+    kategori: ["Makanan", "Daging", "Tradisional"],
+    price: "30000",
+    imageassets: [
+      "assets/images/sate.png",
+    ],
+  ),
+];
